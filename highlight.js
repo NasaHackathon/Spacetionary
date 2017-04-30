b678350b9578a8bc27763e9326fa991d39f8dfce
Mike Liao <mikeliao97@gmail.com>
2017-04-30 01:48:00 -0700

    start impleementing user submitting data

--- highlight.js
+++ highlight.js
@@ -7,7 +7,7 @@ window.terms = [
 
 
 //sidebar
-var sideBarHtmlString = "<div id='sidebar'><div id='term'><h1>TERM</h1> </div><div id='pic'>PICTURE </div><div id='def'><h3>The definition of the sun is this massive red ball whats gucci </h3></div></div>"
+var sideBarHtmlString = "<div id='sidebar'><div id='term'><h1>Sun</h1> </div><div id='pic'>PICTURE </div><div id='def'><h3>The definition of the sun is this massive red ball whats gucci </h3></div></div>"
 var sidebar;
 
 $(document).ready(function() {
@@ -30,8 +30,47 @@ $(document).ready(function() {
   })
 
   $('body').on('click', '#see-more-button', function() {
-    console.log('see more button');
+    const data = window.selectionData;
+    const term = data.term
+    const definition = data.definition;
+    const imgLink = data.imageSource;
+    
+    console.log('data', data);
+    console.log('imglink', imgLink);
+    //Overide the term value
+    sidebar.find('#term').html(`<h1>${term}</h1>`);
+
+    //Overide the image url value
+    sidebar.find('#picDiv').html(`<img id='pic' src='${imgLink}'>`);
+
+    //Override the definiition value
+    sidebar.find('#def').html(`<h3> ${definition} </h3>`);
+    
     sidebar.show();
+  })
+
+
+  /*
+  Will submit information in the form 
+  {
+    term: 'sun',
+    email: 'bobby',
+    definition: 'the sun is a big blob of swag'
+  }
+  */ 
+  $('body').on('click', '#submit-def', function() {
+    console.log('trying to submit definittion');
+    window.email = 'bobby@gmail.com' //haven't figured out how email
+    var email = window.email;
+
+    //term 
+    var term = $('#term').find('h1').html();    
+
+    var definition = $('#textAreaDefinition').val();
+    console.log('definition', definition);
+
+
+    var type = 'submitDefiniton'
   })
 
 });
